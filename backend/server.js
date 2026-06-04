095bc7a96d21586de6acd63ef0681056c222dc0f
Gravyvogan <gravyvogan@gmail.com>
2026-06-04 15:49:55 +0100

    Add server entry point

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const app = require('./src/app');
+const connectDB = require('./src/config/database');
+const logger = require('./src/utils/logger');
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
+
+// Start server
+const server = app.listen(PORT, () => {
+  logger.info(`🚀 Server running on port ${PORT}`);
+  logger.info(`Environment: ${process.env.NODE_ENV || 'development'}`);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  logger.error('UNHANDLED REJECTION! Shutting down...');
+  logger.error(err);
+  process.exit(1);
+});
+
+// Handle uncaught exceptions
+process.on('uncaughtException', (err) => {
+  logger.error('UNCAUGHT EXCEPTION! Shutting down...');
+  logger.error(err);
+  process.exit(1);
+});
+
+module.exports = server;
